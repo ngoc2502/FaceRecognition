c632416dadc85c6c21f51557c9c416c238ac230a
ngoc2502 <58940471+ngoc2502@users.noreply.github.com>
2022-10-14 08:40:24 +0700

    Update app.py
    Signed-off-by: ngoc2502 <58940471+ngoc2502@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask,render_template,Response
+from PIL import Image
+from flask import Flask, jsonify,render_template, request
+from flask_jsglue import JSGlue
+import base64
+import io
+import numpy as np
 
 import cv2
 import os
 
 app=Flask(__name__)
+jsglue = JSGlue(app)
+
 labels=[]
 
 for labl in os.listdir("images"):
@@ -11,41 +18,80 @@ for labl in os.listdir("images"):
 
 path='images'
 
-def generate_frames():
+recog=cv2.face.LBPHFaceRecognizer_create()
+recog.read("training_index.yml")
+facedetect=cv2.CascadeClassifier('haarcascade_frontalFace_default.xml')
+
+def generate_frames(frame):
     recog=cv2.face.LBPHFaceRecognizer_create()
     recog.read("training_index.yml")
-
-    video=cv2.VideoCapture(2)
     facedetect=cv2.CascadeClassifier('haarcascade_frontalFace_default.xml')
-    
 
-    while True:
-        ret,frame=video.read()
-        RGB_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        faces=facedetect.detectMultiScale(RGB_frame, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100))
-        for x,y,w,h in faces:
-
+    Gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    print("RGB : ",Gray_frame.shape)
+    faces=facedetect.detectMultiScale(Gray_frame, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100))
+    for x,y,w,h in faces:
+            print("Finded Ur Face")
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
-            name_id,acc=recog.predict(RGB_frame[y:y+h,x:x+w])
-
+            name_id,acc=recog.predict(Gray_frame[y:y+h,x:x+w])
             if name_id:
                 cv2.putText(frame,labels[name_id] + str(round(acc,2)),(x,y-4),cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,255,0),1,cv2.LINE_AA)
+                print("ID :",labels[name_id])
             else:
                 cv2.putText(frame,"Unknow",(x,y-4),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,0,0),1,cv2.LINE_AA)    
-        
-        r,buffer=cv2.imencode('.jpg',frame)
-        frame=buffer.tobytes()
-        yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n'+frame+b'\r\n')
+                print("Unknow")
+
+    r,buffer=cv2.imencode('.jpg',frame)
+    frame=buffer.tobytes()
+    # yield (labels[name_id])
+    # yield(b'--frame\r\n'
+    #     b'Content-Type: image/jpeg\r\n\r\n'+frame+b'\r\n')
+    return frame
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/get_video_feed',methods=["POST","GET"])
+def get_video_feed():
+    print('==================================')
+    if request.method=='POST':
+        str_dta=request.data.decode("UTF-8")
+        code_data=str_dta.split(',')[1]
+        b64_data=base64.b64decode(code_data)
+            
+        image=Image.open(io.BytesIO(b64_data))
+        image=image.convert('RGB')
+
+        frame=np.array(image)
+      
+        Gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        print("Gray image : ",Gray_frame.shape)
+
+        faces=facedetect.detectMultiScale(Gray_frame, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100))
+
+        for x,y,w,h in faces:
+                print("Finded Ur Face")
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+                name_id,acc=recog.predict(Gray_frame[y:y+h,x:x+w])
+                if name_id:
+                    cv2.putText(frame,labels[name_id] + str(round(acc,2)),(x,y-4),cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,255,0),1,cv2.LINE_AA)
+                    print("ID :",labels[name_id])
+                else:
+                    cv2.putText(frame,"Unknow",(x,y-4),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,0,0),1,cv2.LINE_AA)    
+                    print("Unknow")
+
+        r,buffer=cv2.imencode('.jpg',frame)
+        image=buffer.tobytes()
+        image=base64.b64encode(image).decode("UTF-8")
+        print(len(image))
+        return jsonify({'image':image})
+    else:
+        return render_template('index.html')
+
 
 if __name__=='__main__':
     app.run(debug=True)
+
